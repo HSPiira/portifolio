088f906e563416d6e6c8d8c824e59e35bf3eab09
Henry Piira <sekiboh@gmail.com>
2025-02-02 15:15:27 +0300

    fix: remove output mode setting from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ import remarkGfm from "remark-gfm";
 import rehypePrism from "@mapbox/rehype-prism";
 
 const nextConfig = {
-  output: "export",
   images: {
     domains: ["images.unsplash.com", "res.cloudinary.com"],
   },
